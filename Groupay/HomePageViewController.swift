2a0e100877a111c15aba8fdf4d14b505cf0e766b
Marcus Norona <marcusjeremynorona@gmail.com>
2017-10-09 17:18:46 -0700

    working segues and added mapView

--- Groupay/HomePageViewController.swift
+++ Groupay/HomePageViewController.swift
@@ -7,13 +7,31 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class HomePageViewController: UIViewController {
+    
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager: CLLocationManager!
+    var currentLocation: CLLocation?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+        centerMapOnLocation(location: initialLocation)
     }
+    
+    let regionRadius: CLLocationDistance = 1000
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius * 2.0, regionRadius * 2.0)
+
+    }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
